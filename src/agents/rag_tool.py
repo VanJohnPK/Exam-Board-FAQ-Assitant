5330018d19b8c17858c5e5b365077530885a02b7
darkVinci <1585781755@qq.com>
2025-03-20 19:28:55 +0800

    大修

--- src/agents/rag_tool.py
+++ src/agents/rag_tool.py
@@ -2,52 +2,42 @@ from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import DashScopeEmbeddings
 from dotenv import load_dotenv
 from pydantic import BaseModel, Field
-from langchain_core.tools import Tool
+from langchain_core.tools import StructuredTool
+from typing import Literal
 load_dotenv()
 
 # 初始化嵌入函数
 embeddings = DashScopeEmbeddings(model="text-embedding-v3")
 
-class RetrieverInput(BaseModel):
+class GaoKaoInput(BaseModel):
+    """Input to the retriever."""
+    query: str = Field(description="query to look up in retriever")
+    question_class: Literal["秋考", "春考", "艺术类统一考试", "体育类统一考试", "三校生高考","专科自主招生","高中学业水平考试","中职校学业水平考试","专升本考试","普通高校联合招收华侨港澳台考试", "中考中招"] = Field(description="问题的类别")
+
+class ZhongKaoInput(BaseModel):
     """Input to the retriever."""
     query: str = Field(description="query to look up in retriever")
 
 # 假设 vector_db 已经存在，直接加载
-vectorstore = Chroma(
-    collection_name="rag-chroma",
+vectorstore_gaokao = Chroma(
+    collection_name="rag-chroma-gaokao",
     embedding_function=DashScopeEmbeddings(model="text-embedding-v3"),
-    persist_directory="./vector_db"
+    persist_directory="./vector_db_gaokao"
 )
 
-# 定义关键词到筛选值的映射
-keyword_mapping = {
-    "高考": "高考学考",
-    "春考": "高考学考",
-    "秋考": "高考学考",
-    "强基": "高考学考",
-    "初中": "中考中招",
-    "中考": "中考中招",
-    "考研": "研考成考",
-    "四级": "证书考试",
-    "六级": "证书考试",
-}
-
-def search_with_dynamic_filter(query, search_key="部分", k=4):
+def search_with_dynamic_filter(query, question_class, search_key="考试类型", k=4):
     # 提取关键词
-    relevant_keywords = [keyword for keyword in keyword_mapping if keyword in query]
-
-    if not relevant_keywords:
+    if not question_class:
         # 若未找到关键词，不应用筛选条件
+        print("无question_class")
         filter_condition = None
     else:
-        # 根据映射转换关键词为筛选值
-        filter_values = [keyword_mapping[keyword] for keyword in relevant_keywords]
-        print(filter_values)
+        print("有question_class"+question_class)
         # 构建筛选条件
-        filter_condition = {search_key: {"$in": filter_values}}
+        filter_condition = {search_key: question_class}
 
     # 进行相似性搜索并应用筛选条件
-    results = vectorstore.similarity_search(
+    results = vectorstore_gaokao.similarity_search(
         query,
         k=k,
         filter=filter_condition
@@ -59,10 +49,26 @@ def search_with_dynamic_filter(query, search_key="部分", k=4):
     # 返回一个二元组
     return (content_str, page_contents)
 
-retriever_tool = Tool(
-    name="Exam-Board-FAQ",
+gaokao_tool = StructuredTool(
+    name="GaoKao-FAQ",
     func=search_with_dynamic_filter,
-    description="在文件中查询高考学考、中考中招、研考成考、自学考试和证书考试相关问题回答。",
-    args_schema=RetrieverInput, 
+    description="查询高考学考相关问答，包含秋考、春考、艺术类统一考试、体育类统一考试、三校生高考、专科自主招生、高中学业水平考试、中职校学业水平考试、专升本考试、普通高校联合招收华侨港澳台考试。",
+    args_schema=GaoKaoInput, 
     response_format="content_and_artifact"
+)
+
+from langchain.tools.retriever import create_retriever_tool
+
+vectorstore_zhongkao = Chroma(
+    collection_name="rag-chroma-zhongkao",
+    embedding_function=DashScopeEmbeddings(model="text-embedding-v3"),
+    persist_directory="./vector_db_zhongkao"
+)
+
+retriever = vectorstore_zhongkao.as_retriever()
+
+zhongkao_tool = create_retriever_tool(
+    retriever,
+    "ZhongKao-FAQ",
+    "查询中考中招相关问答。",
 )
